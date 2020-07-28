bd60164e0c11768c282396bf20e1c7fad59ff01e
mangesh\mangeshlap <ozardekar.mangesh@gmail.com>
2020-07-28 17:07:46 +0530

    Initial commit

---
+++ app/src/main/java/com/jet2/assignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jet2.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
